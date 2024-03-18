7428d69c584564e47113e1095ecfac91be36af35
pandekar <pandeadhistanaya@gmail.com>
2024-03-18 21:12:50 +0700

    chapter 15: Adding Authentication

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -4,6 +4,8 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 
 export type State = {
   errors?: {
@@ -111,6 +113,26 @@ const deleteInvoice = async (id: string) => {
     return { message: 'Invoice Deleted' };
   } catch (error) {
     return { message: 'Database error: fail to delete invoice' };
+  }
+}
+
+export async function authenticate (
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+
+    throw error;
   }
 }
 
